0e3740e5e30807e415f716b237553945a720febd
neha-industries-777 <rajputsimran15023010@gmail.com>
2026-05-03 06:19:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll(".nav-link");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+        links.forEach(l => l.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
